2678c3458538ced6b6c64bbf78efc0a11689d841
Martin Hujer <mail@martinhujer.cz>
2023-03-02 11:26:44 +0100

    [ci skip] random card

--- src/Card.tsx
+++ src/Card.tsx
@@ -28,14 +28,21 @@ export class Card {
 }
 
 /**
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * Algorithm taken from the sources of the original game
  */
-function getRandomInt(max: number): number {
-    return Math.floor(Math.random() * max);
+function getRandomCardDefinitionIndex(): number {
+    // non-linear randomness - most of the cards will the ones with lower number
+    // CZ orig: nelineární náhodnost karty - nejvíce bude karet s nízkým číslem
+    const cardNumber = Math.floor(Math.pow(Math.random(), 1.6) * 30);
+
+    // The cards are grouped by type, the weak ones are starting from 0, from 10 and from 20, so this formula handles
+    // choosing from them as if they were sorted by power
+    // CZ orig: změna organizace pořadí karet - 3 skupiny od nejslabších po nejsilnější karty
+    return Math.floor(cardNumber / 3) + (cardNumber % 3) * 10;
 }
 
 export function generateCard(): Card {
-    const randomCardDefinitionIndex = getRandomInt(cardDefinitions.length);
+    const randomCardDefinitionIndex = getRandomCardDefinitionIndex();
 
     const randomCardDefinition = cardDefinitions[randomCardDefinitionIndex];
     if (randomCardDefinition === undefined) {
--- src/Castle.tsx
+++ src/Castle.tsx
@@ -7,14 +7,12 @@ interface Props {
 
 export const Castle: React.FC<Props> = ({ castle, wall }) => {
     const castleCss = {
-        padding: '2em',
         border: '1px solid gray',
         background: 'silver',
         width: '8em',
     };
     const castleHeight = castle * 5;
     const wallCss = {
-        padding: '1em',
         border: '1px solid black',
         background: 'brown',
         width: '1em',
